109684640cda08ea82568ee9fc99ed6f90c82396
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-07 21:23:17 +0000

    Add index.tsx file with initial code

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Index = () => {
+	return (
+		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+			<Text>Hello World</Text>
+		</View>
+	);
+};
+
+export default Index;
