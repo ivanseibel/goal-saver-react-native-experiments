6f35b524394499c78f041eacf15f2ec129b9557b
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-08 17:34:57 +0000

    Add GoalDTO and TransactionDTO to dtos.d.ts

---
+++ src/types/dtos.d.ts
@@ -0,0 +1,13 @@
+type GoalDTO = {
+	id: string;
+	name: string;
+	total: number;
+	current: number;
+};
+
+type TransactionDTO = {
+	id: string;
+	amount: number;
+	goal_id: number;
+	created_at: number;
+};
