d748318b50636fc0ccd0369ae13be0915f6afcc3
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-08 15:52:58 +0000

    Add global styles and fonts to layout component

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,26 @@
+import "@/styles/global.css";
+
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
+import {
+	useFonts,
+	OpenSans_400Regular,
+	OpenSans_600SemiBold,
+	OpenSans_700Bold,
+} from "@expo-google-fonts/open-sans";
+
 const Layout = () => {
+	const [fontsLoaded] = useFonts({
+		OpenSans_400Regular,
+		OpenSans_600SemiBold,
+		OpenSans_700Bold,
+	});
+
+	if (!fontsLoaded) {
+		return null;
+	}
+
 	return (
 		<>
 			<StatusBar style="light" />
