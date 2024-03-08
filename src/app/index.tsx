31124a9e5b925098b4ebfab198f4596d30be808c
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-08 18:03:35 +0000

    Add useEffect hook to fetch goals from mocks

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,30 @@
 import { Goals } from "@/components/Goals";
 import { Header } from "@/components/Header";
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { View } from "react-native";
+import { mocks } from "@/utils/mocks";
 
 const Index = () => {
+	const [goals, setGoals] = useState<GoalDTO[]>([]);
+
+	useEffect(() => {
+		const fetchGoals = async () => {
+			try {
+				const response = mocks.goals;
+				setGoals(response);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		fetchGoals();
+	}, []);
+
 	return (
 		<View className="flex-1 bg-gray-600 py-9 px-8">
 			<Header title="Goal Saver" subtitle="Save today, enjoy tomorrow." />
 
-			<Goals goals={[]} onPress={() => {}} onAdd={() => {}} />
+			<Goals goals={goals} onPress={() => {}} onAdd={() => {}} />
 		</View>
 	);
 };
