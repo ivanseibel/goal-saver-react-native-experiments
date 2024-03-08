fa4f0c52c68615b25caa362ad889473ef4778282
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-08 18:28:18 +0000

    Refactor goals array in mocks.ts

--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -22,7 +22,32 @@ const goal = {
 	transactions,
 };
 
-const goals = [goal];
+const goals = [
+	{
+		id: "1",
+		name: "Computer",
+		current: 2500,
+		total: 3000,
+		percentage: (2500 / 3000) * 100,
+		transactions,
+	},
+	{
+		id: "2",
+		name: "Car",
+		current: 1000,
+		total: 8000,
+		percentage: (1000 / 8000) * 100,
+		transactions,
+	},
+	{
+		id: "3",
+		name: "House",
+		current: 5000,
+		total: 10000,
+		percentage: (5000 / 10000) * 100,
+		transactions,
+	},
+];
 
 export const mocks = {
 	goal,
