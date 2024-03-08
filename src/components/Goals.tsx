9db1ced57ab0a29cf69b87d2ff7ddd3a1b0c9f01
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-08 18:03:44 +0000

    Add horizontal scroll and display goals in Goals component

--- src/components/Goals.tsx
+++ src/components/Goals.tsx
@@ -1,6 +1,7 @@
 import { colors } from "@/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
 import { ScrollView, TouchableOpacity } from "react-native";
+import { Goal } from "./Goal";
 
 type ComponentProps = {
 	goals: GoalDTO[];
@@ -10,7 +11,12 @@ type ComponentProps = {
 
 export const Goals = ({ goals, onPress, onAdd }: ComponentProps) => {
 	return (
-		<ScrollView className="mt-11" contentContainerClassName="h-[164px] gap-4">
+		<ScrollView
+			className="mt-11"
+			contentContainerClassName="h-[164px] gap-4"
+			horizontal
+			showsHorizontalScrollIndicator={false}
+		>
 			{/* Add button */}
 			<TouchableOpacity
 				activeOpacity={0.7}
@@ -19,6 +25,11 @@ export const Goals = ({ goals, onPress, onAdd }: ComponentProps) => {
 			>
 				<MaterialIcons name="add" size={36} color={colors.black} />
 			</TouchableOpacity>
+
+			{/* Goals */}
+			{goals.map((goal) => (
+				<Goal key={goal.id} goal={goal} onPress={() => onPress(goal.id)} />
+			))}
 		</ScrollView>
 	);
 };
