32441f20ea03c3b6db06313cd87717126e563121
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-08 18:28:14 +0000

    Add ProgressBar component

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,26 @@
+import { View, Text } from "react-native";
+
+type ComponentProps = {
+	percentage: number;
+};
+
+export const ProgressBar = ({ percentage }: ComponentProps) => {
+	const value = `${percentage.toFixed(0)}%`;
+	const width = percentage > 100 ? 100 : percentage;
+	return (
+		<View className="w-full h-7 rounded-full bg-gray-400 overflow-hidden flex-row items-center">
+			<View
+				className="h-7 items-end justify-center rounded-full bg-green-500"
+				style={{ width: `${width}%` }}
+			>
+				{percentage >= 60 && (
+					<Text className="text-black text-xs font-semiBold mx-5">{value}</Text>
+				)}
+			</View>
+
+			{percentage < 60 && (
+				<Text className="text-white text-xs font-semiBold mx-5">{value}</Text>
+			)}
+		</View>
+	);
+};
