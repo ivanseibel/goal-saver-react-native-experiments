ae810a4c1ed22267f94ed795a5c03625beda3371
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-11 17:27:57 +0000

    Add bottom sheet component and handle open/close events

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,19 @@
 import { Goals } from "@/components/Goals";
 import { Header } from "@/components/Header";
-import { useEffect, useState } from "react";
-import { View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { View, Text } from "react-native";
 import { mocks } from "@/utils/mocks";
 import { Transactions } from "@/components/Transactions";
+import { BottomSheet } from "@/components/BottomSheet";
+import BottomSheetComponent from "@gorhom/bottom-sheet";
+import { Input } from "@/components/Input";
 
 const Index = () => {
 	const [goals, setGoals] = useState<GoalDTO[]>([]);
+	const bottomSheetRef = useRef<BottomSheetComponent>(null);
+
+	const handleBottomSheetOpen = () => bottomSheetRef.current?.expand();
+	const handleBottomSheetClose = () => bottomSheetRef.current?.snapToIndex(0);
 
 	useEffect(() => {
 		const fetchGoals = async () => {
@@ -22,13 +29,28 @@ const Index = () => {
 	}, []);
 
 	return (
-		<View className="flex-1 bg-gray-600 py-9 px-8">
-			<Header title="Goal Saver" subtitle="Save today, enjoy tomorrow." />
+		<>
+			<View className="flex-1 bg-gray-600 py-9 px-8">
+				<Header title="Goal Saver" subtitle="Save today, enjoy tomorrow." />
 
-			<Goals goals={goals} onPress={() => {}} onAdd={() => {}} />
+				<Goals goals={goals} onPress={() => {}} onAdd={handleBottomSheetOpen} />
 
-			<Transactions transactions={mocks.transactions} />
-		</View>
+				<Transactions transactions={mocks.transactions} />
+			</View>
+			<BottomSheet
+				ref={bottomSheetRef}
+				title="New Goal"
+				snapPoints={[0.01, 284]}
+				onClose={handleBottomSheetClose}
+			>
+				<View>
+					<View className="gap-4">
+						<Input placeholder="Goal name" />
+						<Input placeholder="Amount" />
+					</View>
+				</View>
+			</BottomSheet>
+		</>
 	);
 };
 
