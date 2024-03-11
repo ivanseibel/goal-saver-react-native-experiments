1dc4c6d8741aa5c01c0171ccc360071572e8641b
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-11 12:42:29 +0000

    Add Transaction component

---
+++ src/components/Transaction.tsx
@@ -0,0 +1,22 @@
+// import { formatCurrency } from "@/utils/formatCurrency";
+import { formatCurrencyCrossPlatform } from "@/utils/formatCurrency";
+import { View, Text } from "react-native";
+
+type ComponentProps = Omit<TransactionDTO, "goal_id" | "id">;
+
+export const Transaction = ({ amount, created_at }: ComponentProps) => {
+	const textColor = amount > 0 ? "text-green-500" : "text-red-500";
+	const normalizedAmount =
+		amount > 0
+			? `+ ${formatCurrencyCrossPlatform(amount)}`
+			: `- ${formatCurrencyCrossPlatform(amount)}`;
+
+	return (
+		<View className="bg-gray-500 border border-gray-400 rounded h-16 w-full justify-between flex-row items-center px-4">
+			<Text className={`text-white text-base font-semiBold ${textColor}`}>
+				{normalizedAmount}
+			</Text>
+			<Text className="text-gray-300 text-xs font-normal">{created_at}</Text>
+		</View>
+	);
+};
