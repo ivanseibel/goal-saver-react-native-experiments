53094ad4ef44c07b551026ac741d71007e646628
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-12 11:07:34 +0000

    Add Details component with goal details and bottom sheet

---
+++ src/app/details/[id].tsx
@@ -0,0 +1,62 @@
+import { BackButton } from "@/components/BackButton";
+import { BottomSheet } from "@/components/BottomSheet";
+import BottomSheetComponent from "@gorhom/bottom-sheet";
+import { Button } from "@/components/Button";
+import { Header } from "@/components/Header";
+import { Input } from "@/components/Input";
+import { ProgressBar } from "@/components/ProgressBar";
+import { Transactions } from "@/components/Transactions";
+import { mocks } from "@/utils/mocks";
+import { useRef, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { colors } from "@/styles/colors";
+
+const Details = () => {
+	const [amount, setAmount] = useState("");
+
+	const bottomSheetRef = useRef<BottomSheetComponent>(null);
+	const handleBottomSheetOpen = () => bottomSheetRef.current?.expand();
+	const handleBottomSheetClose = () => bottomSheetRef.current?.snapToIndex(0);
+
+	return (
+		<>
+			<View className="flex-1 bg-gray-600 pt-9 pb-4 px-8 gap-7 relative">
+				<BackButton />
+
+				<Header title="My Goal" subtitle="EUR 1,000.00 of EUR 5,000.00 saved" />
+
+				<ProgressBar percentage={20} />
+
+				<Transactions transactions={mocks.transactions} />
+
+				<TouchableOpacity
+					className="absolute bottom-4 right-4 bg-green-500 rounded-full w-16 h-16 items-center justify-center"
+					onPress={handleBottomSheetOpen}
+				>
+					<MaterialIcons name="add" size={24} color={colors.black} />
+				</TouchableOpacity>
+			</View>
+			<BottomSheet
+				ref={bottomSheetRef}
+				title="New Goal"
+				snapPoints={[0.001, 200]}
+				onClose={handleBottomSheetClose}
+			>
+				<View>
+					<View className="gap-4">
+						<Input
+							placeholder="Amount"
+							onChangeText={(text) => setAmount(text)}
+							value={amount}
+						/>
+
+						<Button label="Create" />
+					</View>
+				</View>
+			</BottomSheet>
+		</>
+	);
+};
+
+export default Details;
