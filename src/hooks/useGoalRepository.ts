c352bd2b69cc38f833e523a9dca0718ae46d09c4
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-14 17:41:35 +0000

    Refactor useGoalRepository hooks

--- src/hooks/useGoalRepository.ts
+++ src/hooks/useGoalRepository.ts
@@ -3,15 +3,15 @@ import { useSQLiteContext } from "expo-sqlite/next";
 export function useGoalRepository() {
 	const database = useSQLiteContext();
 
-	async function createGoal(name: string, total: number) {
+	const createGoal = (name: string, total: number) => {
 		const statement = database.prepareSync(
 			"INSERT INTO goals (name, total) VALUES ($name, $total)",
 		);
 
 		statement.executeSync({ $name: name, $total: total });
-	}
+	};
 
-	function getGoals() {
+	const getGoals = () => {
 		const result = database.getAllSync<GoalDTO>(`
       SELECT 
         g.id, 
@@ -24,7 +24,25 @@ export function useGoalRepository() {
     `);
 
 		return result;
-	}
+	};
 
-	return { createGoal, getGoals };
+	const getGoal = (id: string) => {
+		const statement = database.prepareSync(`
+			SELECT 
+				g.id, 
+				g.name, 
+				g.total, 
+				COALESCE(SUM(t.amount), 0) AS current
+			FROM goals AS g
+			LEFT JOIN transactions t ON t.goal_id = g.id
+			WHERE g.id = $id
+			GROUP BY g.id, g.name, g.total
+		`);
+
+		const result = statement.executeSync<GoalDTO>({ $id: id });
+
+		return result.getFirstSync();
+	};
+
+	return { createGoal, getGoals, getGoal };
 }
