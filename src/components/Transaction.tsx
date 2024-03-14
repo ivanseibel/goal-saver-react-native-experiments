7539c4accb61adb481bc120c2a21b8e2b3f928ba
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-14 21:08:28 +0000

    Remove text color class from Transaction component

--- src/components/Transaction.tsx
+++ src/components/Transaction.tsx
@@ -13,7 +13,7 @@ export const Transaction = ({ amount, created_at }: ComponentProps) => {
 
 	return (
 		<View className="bg-gray-500 border border-gray-400 rounded h-16 w-full justify-between flex-row items-center px-4">
-			<Text className={`text-white text-base font-semiBold ${textColor}`}>
+			<Text className={`text-base font-semiBold ${textColor}`}>
 				{normalizedAmount}
 			</Text>
 			<Text className="text-gray-300 text-xs font-normal">{created_at}</Text>
